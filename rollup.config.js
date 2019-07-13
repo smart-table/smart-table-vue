de8600765a38fef0b08ce0c1e550c5b20db3b700
Jan Edmund Lazo <jan.lazo@mail.utoronto.ca>
2019-07-13 12:55:24 -0400

    Fix rollup warning with missing output.globals

--- rollup.config.js
+++ rollup.config.js
@@ -1,16 +1,19 @@
 import nodeResolve from 'rollup-plugin-node-resolve'
 
+const globals = {
+  'smart-table-core': 'smartTableCore',
+};
+
 export default {
-  external: [
-    'smart-table-core',
-  ],
   input: './index.js',
   output: {
     file: './dist/smart-table-vue.js',
     format: 'umd',
+    globals,
     name:'smart-table-vue',
     sourcemap: true,
   },
+  external: Object.keys(globals),
   plugins: [
     nodeResolve(),
   ]
