d0d65341ab99b4ebf80f2d0c2b4d0fbee1922303
Jan Edmund Lazo <jan.lazo@mail.utoronto.ca>
2019-07-15 00:40:12 -0400

    Add first/last buttons in pagination example

--- example/components/pagination.vue
+++ example/components/pagination.vue
@@ -1,8 +1,10 @@
 <template>
   <td colspan="2">
+    <button :disabled="isPreviousDisabled" @click="selectPage(1)">1</button>
     <button :disabled="isPreviousDisabled" v-on:click="selectPreviousPage">Previous</button>
     <span> - Page {{stState.page}} - </span>
     <button :disabled="isNextDisabled" v-on:click="selectNextPage">Next</button>
+    <button :disabled="isNextDisabled" @click="selectPage(pageCount)">{{pageCount}}</button>
   </td>
 </template>
 
@@ -14,12 +16,14 @@
       return {
         isPreviousDisabled: true,
         isNextDisabled: true,
+        pageCount: 1,
       }
     },
     watch: {
       stState: function () {
         this.isPreviousDisabled = !this.stDirective.isPreviousPageEnabled();
         this.isNextDisabled = !this.stDirective.isNextPageEnabled();
+        this.pageCount = this.stDirective.state().pageCount;
       }
     }
   }
